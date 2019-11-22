53d890e02274ea270d4d8b38d4c8fdd028a4b4d8
mzukovec <mzukovec@mzukovec-VirtualBox>
2019-11-22 14:33:58 +0100

    for while sum

--- Empty C++ project/ch1_prog_1.cpp
+++ Empty C++ project/ch1_prog_1.cpp
@@ -1,28 +1,21 @@
 #include <iostream>
 int main()
 {
-    int sum = 0, val = 1;
-    while (val <= 10)
+    int sum = 0;
+    for (int i = 50; i <= 100; ++i)
     {
-    	sum += val;
-    	++val;
+    	sum += i;
     }
-    std::cout << "Sum of 1 to 10, inclusive is " << sum << std::endl;
+    std::cout << "The sum from 50 to 100 is: " << sum << std::endl;
 
-    int sum2 = 0;
-    for (int val2 = 1; val2 <= 10; ++val2)
+    int sum2 = 0, i=50;
+    while (i <= 100)
     {
-        sum2 += val2;
-    }
-    std::cout << "Sum of 1 to 10 inclusive is " << sum2 << std::endl;
-
-    int sum3 = 0;
-    for (int i = -100; i <= 100; ++i)
-    {
-    	sum3 += i;
+    	sum2 += i;
+    	i += 1;
     }
 
-    std::cout << "The sum from -100 to 100 in for loop is: " << sum3 << std::endl;
+    std::cout << "The sum from 50 to 100 is: " << sum << std::endl;
 
     return 0;
 }
