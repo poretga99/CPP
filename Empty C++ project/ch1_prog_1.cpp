fa779c58f79b2a719cf2af92bd9ef0209768714d
mzukovec <mzukovec@mzukovec-VirtualBox>
2019-11-22 14:37:21 +0100

    For and while from 10 to 0

--- Empty C++ project/ch1_prog_1.cpp
+++ Empty C++ project/ch1_prog_1.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 int main()
 {
-    int sum = 0;
-    for (int i = 50; i <= 100; ++i)
+    int num = 10;
+    while (num >= 0)
     {
-    	sum += i;
-    }
-    std::cout << "The sum from 50 to 100 is: " << sum << std::endl;
-
-    int sum2 = 0, i=50;
-    while (i <= 100)
-    {
-    	sum2 += i;
-    	i += 1;
+    	std::cout << num << std::endl;
+    	num -= 1;
     }
 
-    std::cout << "The sum from 50 to 100 is: " << sum << std::endl;
 
+    std::cout << "Using for loop: " << std::endl;
+
+    for (int i = 10; i >= 0; --i)
+    {
+    	std::cout << i << std::endl;
+    }
     return 0;
 }
