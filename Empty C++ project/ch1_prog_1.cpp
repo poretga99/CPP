c92dd023aa25fa3539d2628624390594cba86983
mzukovec <mzukovec@mzukovec-VirtualBox>
2019-11-22 14:08:27 +0100

    Finished chapter 1, exercises explanied inside of ch1_prog_1.coo file

--- Empty C++ project/ch1_prog_1.cpp
+++ Empty C++ project/ch1_prog_1.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 int main()
 {
-	std::cout << "Enter two numbers:" << std::endl;
-	int v1, v2;
-	std::cin >> v1 >> v2;
-	std::cout << "The sum of " << v1 << " and " << v2
+    std::cout << "Enter two numbers:" << std::endl;
+    int v1, v2;
+    std::cin >> v1 >> v2;
+    std::cout << "The sum of " << v1 << " and " << v2
 			  << " is " << v1 + v2 << std::endl;
-	return 0;
+    // 1.3 naloga
+    std::cout << "Hello, World!" << std::endl;
+    // 1.4 naloga
+    std::cout << "Enter two numbers again:" << std::endl;
+    int u1, u2;
+    std::cin >> u1 >> u2;
+    std::cout << "The product of " << u1 << " and " << u2 << " is " << u1 * u2 << std::endl;
+    return 0;
+    //1.5 not legal, we dont use any operand after ;
 }
