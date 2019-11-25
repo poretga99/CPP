020606a9a388efccc2787cf1f8f58271cf1858cf
mzukovec <mzukovec@mzukovec-VirtualBox>
2019-11-25 12:29:02 +0100

    Sizes of different types

--- Empty C++ project/ch1_prog_1.cpp
+++ Empty C++ project/ch1_prog_1.cpp
@@ -1,33 +1,22 @@
 #include <iostream>
-
-double sqrt(double d)
-    {
-        return 0;
-    }
-int error_number = 1;
-struct User
-{
-    int weight;
-    int height;
-};
-struct Date
-{
-    int d, m, y;
-};
-
-int day(Date* p)
-{
-    return p->d;
-}
-
+#include <limits>
+#include <climits>
 int main()
 {
-    int count;
-    char* name;
-    Date dt1;
-
-    double a = sqrt(2);
-    User martin;
-    extern int error_number;
+    enum e1 { a=3, b=9};
+    int* p;
+    // char has 8 bits, sizeof(type) returns size in char character, so bits=sizeof(type)*8bit
+    std::cout << "Size of type bool is: " << sizeof(bool) << std::endl;
+    std::cout << "Size of type char is: " << sizeof(char) << std::endl;
+    std::cout << "Size of type short is: " << sizeof(short) << std::endl;
+    std::cout << "Size of type int is: " << sizeof(int) << std::endl;
+    std::cout << "Size of type long is: " << sizeof(long) << std::endl;
+    std::cout << "Size of type float is: " << sizeof(float) << std::endl;
+    std::cout << "Size of type double is: " << sizeof(double) << std::endl;
+    std::cout << "Size of type long double is: " << sizeof(long double) << std::endl;
+    std::cout << "Size of type unsigned is: " << sizeof(unsigned) << std::endl;
+    std::cout << CHAR_BIT << std::endl;
+    std::cout << "Size of type int pointer is: " << sizeof(p) << std::endl; //fixed 8 byte on x64?
+    std::cout << "Size of enumerator e1 is:" << sizeof(e1) << std::endl; //min(e1) = 9 -> nearest 2**n - 1 -> 2**4 - 1
     return 0;
 }
