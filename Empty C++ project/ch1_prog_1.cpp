689e87a5e8bf631a23f7eebfc86228d01b4b5330
mzukovec <mzukovec@mzukovec-VirtualBox>
2019-11-25 11:16:57 +0100

    4.11 ex 2

--- Empty C++ project/ch1_prog_1.cpp
+++ Empty C++ project/ch1_prog_1.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+
+double sqrt(double d)
+    {
+        return 0;
+    }
+int error_number = 1;
+struct User
+{
+    int weight;
+    int height;
+};
+struct Date
+{
+    int d, m, y;
+};
+
+int day(Date* p)
+{
+    return p->d;
+}
+
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    int count;
+    char* name;
+    Date dt1;
+
+    double a = sqrt(2);
+    User martin;
+    extern int error_number;
     return 0;
 }
