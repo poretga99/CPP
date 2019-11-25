aa2a1d514a4b0674c2294ce86341959bb0c1d7e2
mzukovec <mzukovec@mzukovec-VirtualBox>
2019-11-25 10:10:56 +0100

    started with ch 3 of c++ academy

--- Empty C++ project/ch1_prog_1.cpp
+++ Empty C++ project/ch1_prog_1.cpp
@@ -1,18 +1,6 @@
 #include <iostream>
-#include <stdlib.h> //abs
-#include "Sales_item.h"
 int main()
 {
-	Sales_item item1, item2;
-    std::cin >> item1 >> item2;
-    if (item1.isbn() == item2.isbn())
-    {
-    	std::cout << item1 + item2 << std::endl;
-    	return 0;
-    }
-    else
-    {
-    	std::cerr << "data must refer to same ISBN" << std::endl;
-    	return -1;
-    }
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
 }
