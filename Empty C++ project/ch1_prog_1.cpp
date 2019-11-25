77d2c56e34dce8fc6d14bd4fc39fd45bf012c42d
mzukovec <mzukovec@mzukovec-VirtualBox>
2019-11-25 09:28:16 +0100

    ISBN error

--- Empty C++ project/ch1_prog_1.cpp
+++ Empty C++ project/ch1_prog_1.cpp
@@ -3,8 +3,16 @@
 #include "Sales_item.h"
 int main()
 {
-	Sales_item book;
-	std::cin >> book;
-	std::cout << book << std::endl;
-    return 0;
+	Sales_item item1, item2;
+    std::cin >> item1 >> item2;
+    if (item1.isbn() == item2.isbn())
+    {
+    	std::cout << item1 + item2 << std::endl;
+    	return 0;
+    }
+    else
+    {
+    	std::cerr << "data must refer to same ISBN" << std::endl;
+    	return -1;
+    }
 }
