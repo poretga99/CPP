9e20186c24cd6fd688f1d9dc6bb1f1a0090a79ba
mzukovec <mzukovec@mzukovec-VirtualBox>
2019-11-25 09:05:40 +0100

    Sales_item implementation

--- Empty C++ project/ch1_prog_1.cpp
+++ Empty C++ project/ch1_prog_1.cpp
@@ -1,24 +1,10 @@
 #include <iostream>
 #include <stdlib.h> //abs
+#include "Sales_item.h"
 int main()
 {
-	std::cout << "Write pair of numbers." << std::endl;
-	int v1, v2;
-	std::cin >> v1 >> v2;
-	std::cout << "program starting..." << std::endl;
-	int smaller = v1, larger = v2;
-	if (v1 > v2)
-	{
-		smaller = v2;
-		larger = v1;
-	}
-	int count = 1;
-	while (smaller <= larger && count <= 10)
-	{
-		std::cout << smaller << std::endl;
-		smaller += 1;
-		count += 1;
-	}
-
+	Sales_item book;
+	std::cin >> book;
+	std::cout << book << std::endl;
     return 0;
 }
