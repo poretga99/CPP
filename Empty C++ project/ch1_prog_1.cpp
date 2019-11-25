9002ecb2d6bb71bb01f8709f456447b6c0bc9d0b
mzukovec <mzukovec@mzukovec-VirtualBox>
2019-11-25 08:52:24 +0100

    Write max 10 numbers in a given range

--- Empty C++ project/ch1_prog_1.cpp
+++ Empty C++ project/ch1_prog_1.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
+#include <stdlib.h> //abs
 int main()
 {
-	std::cout << "Enter unknown number of values. Remeber to terminate with CTRL+D!" << std::endl;
-	int sum = 0, value;
-	while (std::cin >> value)
+	std::cout << "Write pair of numbers." << std::endl;
+	int v1, v2;
+	std::cin >> v1 >> v2;
+	std::cout << "program starting..." << std::endl;
+	int smaller = v1, larger = v2;
+	if (v1 > v2)
 	{
-		if (value < 0)
-		{
-			sum += 1;
-		}
+		smaller = v2;
+		larger = v1;
 	}
-	std::cout << "Number of negative values is: " << sum << std::endl;
+	int count = 1;
+	while (smaller <= larger && count <= 10)
+	{
+		std::cout << smaller << std::endl;
+		smaller += 1;
+		count += 1;
+	}
+
     return 0;
 }
